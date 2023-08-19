935480e7b868c6035e8b1bddeaf3f395df569968
GopalAror <punjabijanu586@gmail.com>
2023-08-19 09:42:22 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+let label = document.querySelector("#label") 
+let navbox = document.querySelector(".navbox") 
+label.addEventListener("click" , function (){
+    navbox.classList.toggle("left-0")
+})
